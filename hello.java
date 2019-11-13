6be504cd4455b0393beee06eb7db880998198c5c
nitairoy <nitaishapla@gmail.com>
2019-11-13 21:24:09 +0100

    New display method is added

--- hello.java
+++ hello.java
@@ -2,6 +2,12 @@ public class Hello {
 
     public static void main (String[] args){
         System.out.prinln("VS is really awersome");
+
+        greeting("This is nitai");
+    }
+
+    public static void greeting(String name) {
+        System.out.println("Welcome" + name);
     }
 }
 
